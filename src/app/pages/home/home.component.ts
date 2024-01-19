9eed3761a37d23876a2b1bca36b41df0c9b5cae3
ranvir jha <tappers_callous0h@icloud.com>
2024-01-20 01:58:05 +0530

    cosmetic changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,7 +13,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     currentTheme!: AppTheme | null;
 
     private readonly _themeService = inject(ThemeService);
-
     private readonly _destroy$ = new Subject();
 
     ngOnInit(): void {
