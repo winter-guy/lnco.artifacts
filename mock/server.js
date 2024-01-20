83f4f9f1ff33b0377ae68279764ec5d4763eeddb
ranvir jha <tappers_callous0h@icloud.com>
2024-01-21 04:07:54 +0530

    configured server

--- mock/server.js
+++ mock/server.js
@@ -1,14 +1,26 @@
 express = require('express');
 data = require('./home/data');
 
+fs = require('fs').promises;
+path = require('path');
+
 app = express();
 port = 3000;
 
 app.use(express.json());
 
 app.get('/api/users', (req, res) => {
-  req;
   res.json(data.users);
+});
+
+app.get('/api/articles', async (req, res) => {
+  try {
+    const articlesData = await fs.readFile(path.join(__dirname, './home/article.json'), 'utf-8');
+    res.json(JSON.parse(articlesData));
+  } catch (error) {
+    console.error('Error reading articles.json', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
