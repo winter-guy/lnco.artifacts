7f01881b8db85976d07b1a5503e7dacf56448ed2
ranvir jha <tappers_callous0h@icloud.com>
2024-01-23 01:41:59 +0530

    ++ artifact service

--- src/app/lib/services/artifacts/artifacts.service.ts
+++ src/app/lib/services/artifacts/artifacts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpService } from '../http/http-client-wrapper.service';
 import { Artifact } from '@lib/interfaces/artifact';
 import { Observable } from 'rxjs';
+import { Article } from '@lib/interfaces/article';
 
 @Injectable({
     providedIn: 'root',
@@ -13,8 +14,8 @@ export class ArtifactService {
         return this.httpService.get<Artifact[]>('/articles');
     }
 
-    public getArtifactsById(id: string): Observable<Artifact[]> {
+    public getArtifactsById(id: string): Observable<Article> {
         id = 'a3b9c8d7e6f5a4b3c2d1e0f';
-        return this.httpService.get<Artifact[]>(`/artifacts?id=${id}`);
+        return this.httpService.get<Article>(`/artifacts?id=${id}`);
     }
 }
