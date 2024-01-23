1bd850e11073de66b0f84c987702575f5eee813f
ranvir jha <tappers_callous0h@icloud.com>
2024-01-24 01:30:01 +0530

    artifact --detection method

--- src/app/pages/artifact/artifact.component.ts
+++ src/app/pages/artifact/artifact.component.ts
@@ -5,9 +5,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Article } from '@lib/interfaces/article';
 
 import { ArtifactService } from '@lib/services/artifacts/artifacts.service';
-import { Observable } from 'rxjs';
 
-import { editorjsConfig, toolsConfig } from '@lib/editor/editor.config';
+import { toolsConfig } from '@lib/editor/editor.config';
 import EditorJS from '@editorjs/editorjs';
 import { ActivatedRoute } from '@angular/router';
 
@@ -17,19 +16,17 @@ import { ActivatedRoute } from '@angular/router';
     templateUrl: './artifact.component.html',
 })
 export class ArtifactComponent implements OnInit {
-    public artifact$!: Observable<Article>;
-    public localCached!: Article;
+    public post!: Article | undefined;
     public editor!: EditorJS;
-    public editorObserver!: MutationObserver;
+
     constructor(protected artifactService: ArtifactService, private _router: ActivatedRoute) {}
 
     ngOnInit(): void {
         const url = this._router.snapshot.queryParams['page'] as string;
-        this.artifact$ = this.artifactService.getArtifactsById(url);
 
         this.artifactService.getArtifactsById(url).subscribe((res) => {
             if (res) {
-                this.localCached = res;
+                this.post = res;
                 this.editor = new EditorJS({
                     holder: 'editorjs',
                     autofocus: false,
@@ -39,27 +36,8 @@ export class ArtifactComponent implements OnInit {
                 });
             }
         });
-
-        this.detectEditorChanges();
     }
 
     @ViewChild('editorjs')
     div!: ElementRef<HTMLInputElement>;
-
-    detectEditorChanges(): Observable<unknown> {
-        return new Observable((observer) => {
-            const editorDom = <Element>document.querySelector('#editorjs');
-            console.log(editorDom);
-            const config = { attributes: true, childList: true, subtree: true };
-            this.editorObserver = new MutationObserver((mutation) => {
-                observer.next(mutation);
-            });
-
-            this.editorObserver.observe(editorDom, config);
-        });
-    }
-
-    public buildEditorWithoutBlocks(): void {
-        this.editor = new EditorJS(editorjsConfig);
-    }
 }
