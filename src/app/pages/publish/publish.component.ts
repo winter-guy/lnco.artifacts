fb8af5b4b1b505c7a6803556cd0935671be93b2b
ranvir jha <tappers_callous0h@icloud.com>
2024-01-28 12:47:26 +0530

    publish design change

--- src/app/pages/publish/publish.component.ts
+++ src/app/pages/publish/publish.component.ts
@@ -4,14 +4,23 @@ import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 import { Compose } from '@lib/interfaces/compose';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { Tag } from '@lib/interfaces/article';
+
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { NgFor } from '@angular/common';
+
 @Component({
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, MatChipsModule, MatIconModule, NgFor],
     templateUrl: './publish.component.html',
     styleUrl: './publish.component.css',
 })
 export class PublishComponent implements OnInit {
     public editorForm!: FormGroup;
+    public tags: Tag[] = [];
+    readonly separatorKeysCodes = [ENTER, COMMA] as const;
 
     constructor(
         public dialogRef: DialogRef<string>,
@@ -38,5 +47,21 @@ export class PublishComponent implements OnInit {
 
     public close(): void {
         this.dialogRef.close();
+    }
+
+    public add(event: MatChipInputEvent): void {
+        const value = (event.value || '').trim();
+
+        if (value) {
+            this.tags.push({ name: value });
+        }
+        event.chipInput?.clear();
+    }
+
+    public remove(tag: Tag): void {
+        const index: number = this.tags.indexOf(tag);
+        if (index >= 0) {
+            this.tags.splice(index, 1);
+        }
     }
 }
