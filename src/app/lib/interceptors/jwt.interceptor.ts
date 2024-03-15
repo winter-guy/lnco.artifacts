f470801c34d2a9c88548e304a416d47b50a9e816
ranvir jha <tappers_callous0h@icloud.com>
2024-03-15 05:32:54 +0530

    interceptor config

--- src/app/lib/interceptors/jwt.interceptor.ts
+++ src/app/lib/interceptors/jwt.interceptor.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { environment } from '@env/environment';
-import { AuthService } from '@lib/services';
+import { AuthService } from '@auth0/auth0-angular';
+
+import { switchMap } from 'rxjs/operators';
 
 /**
  * Interceptor that adds an Authorization header to requests that are authenticated and target the API URL.
@@ -14,18 +16,22 @@ import { AuthService } from '@lib/services';
 export const jwtInterceptor: HttpInterceptorFn = (request, next) => {
     const authService = inject(AuthService);
 
-    const isRequestAuthorized = authService.isAuthenticated && request.url.startsWith(environment.apiUri);
+    return authService.getAccessTokenSilently().pipe(
+        switchMap((token) => {
+            const clonedRequest = request.clone({
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                setHeaders: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'Access-Control-Allow-Origin': 'true',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+                    'SPA-name': 'artifacts',
 
-    if (isRequestAuthorized) {
-        const clonedRequest = request.clone({
-            setHeaders: {
-                // eslint-disable-next-line @typescript-eslint/naming-convention
-                Authorization: `Bearer ${'JWT TOKEN'}`,
-            },
-        });
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-        return next(clonedRequest);
-    }
-
-    return next(request);
+            return next(clonedRequest);
+        }),
+    );
 };
