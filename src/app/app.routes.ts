aad64d0996247f25fe7dd57b5e7f84c6b8de1162
ranvir jha <tappers_callous0h@icloud.com>
2024-04-16 03:51:50 +0530

    123456b

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,6 @@ export const routes: Routes = [
     {
         path: 'behind-the-scene',
         loadChildren: async () => (await import('@pages/bth')).routes,
-        canActivate: [AuthGuard],
     },
     {
         path: 'journal',
