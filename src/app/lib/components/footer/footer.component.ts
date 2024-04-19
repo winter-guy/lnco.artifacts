b458b217d028f5af53c63f8d46af2cf943dfc4ee
ranvir jha <tappers_callous0h@icloud.com>
2024-04-20 03:06:58 +0530

    server status dialog

--- src/app/lib/components/footer/footer.component.ts
+++ src/app/lib/components/footer/footer.component.ts
@@ -4,11 +4,14 @@ import { RouterModule } from '@angular/router';
 import { PACKAGE_JSON, providePackageJson } from '@lib/providers';
 import { LogoComponent } from '../logo/logo.component';
 import { HttpService } from '@lib/services/http/http-client-wrapper.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { StatusComponent } from './status/status';
+import { HealthService } from '@lib/services/health/health.service';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [CommonModule, RouterModule, LogoComponent],
+    imports: [CommonModule, RouterModule, LogoComponent, MatDialogModule],
     providers: [providePackageJson()],
     templateUrl: './footer.component.html',
     // changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +19,8 @@ import { HttpService } from '@lib/services/http/http-client-wrapper.service';
 export class FooterComponent implements OnInit {
     readonly packageJson = inject(PACKAGE_JSON);
     readonly health = inject(HttpService);
+    readonly matDialog = inject(MatDialog);
+    readonly hs = inject(HealthService);
 
     readonly currentYear = new Date().getFullYear();
 
@@ -26,5 +31,12 @@ export class FooterComponent implements OnInit {
         });
     }
 
+    open(): void {
+        this.matDialog.open(StatusComponent, {
+            data: { status: false },
+            disableClose: false,
+            panelClass: ['rounded'],
+        });
+    }
     //create it through behaviour subject
 }
